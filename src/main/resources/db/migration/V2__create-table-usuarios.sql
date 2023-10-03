8499a7c95a0e8763745f8b8531542639dfea9654
frank-esc <frankyeescobar@hotmail.com>
2023-10-02 21:34:17 -0600

    changing migrations

--- src/main/resources/db/migration/V2__create-table-usuarios.sql
+++ src/main/resources/db/migration/V2__create-table-usuarios.sql
@@ -3,7 +3,7 @@ create table usuarios(
     id bigint not null auto_increment,
     nombre varchar(100) not null,
     email varchar(100) not null unique,
-    contrasena varchar(300) not null,
+    contrasenha varchar(300) not null,
 
     primary key(id)
 
